40f0358542d0178a6b2857198eed15551fd3bca0
Bibek Lamichhane <lamichhane.kebib@gmail.com>
2025-07-22 09:25:11 +0545

    Add main function to execute examples from Basics package

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import basics "github.com/OpenSource-np/go-exercise/Basics"
+
+func main() {
+	basics.Base64Example()
+	basics.JSONExample()
+	basics.HexExample()
+	basics.BasicDefer()
+	basics.DeferMultiple()
+	basics.DeferInFunc()
+}
